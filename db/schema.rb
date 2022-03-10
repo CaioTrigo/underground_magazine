4241c2779dc801fc21c751e074d6fe96ad529b7c
bernardogarbin <bernardo.garbin@gmail.com>
2022-03-10 13:52:23 -0300

    tips migrate created

--- db/schema.rb
+++ db/schema.rb
@@ -26,5 +26,4 @@ ActiveRecord::Schema.define(version: 2022_03_09_141121) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
