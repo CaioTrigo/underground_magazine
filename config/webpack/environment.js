d1c48e49f2c8dd221e99a5e412fbeee6605dd781
Gabriel Schiavo <gabriel.penha@agrotools.com.br>
2022-03-17 11:20:19 -0300

    Add preview photo in new Post form

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide', new webpack.ProvidePlugin({
+  $: 'jquery/src/jquery',
+  jQuery: 'jquery/src/jquery',
+  jquery: 'jquery',
+  'window.jQuery': 'jquery',
+  Popper: ['popper.js', 'default']
+}))
+
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
 module.exports = environment
