2bcfe1aabd9b51817a8f25af6efdca3b48ac5b2e
Marcio Ferreira <marcio.ferreira953@gmail.com>
2022-03-18 12:01:47 -0300

    button like heart

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,3 +1,4 @@
+require "pry"
 class LikesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
@@ -5,6 +6,12 @@ class LikesController < ApplicationController
     like.user = current_user
     like.post = post
     like.save!
+    redirect_to posts_path(anchor: "post-#{post.id}")
+  end
+  def destroy
+    post = Post.find(params[:id].to_i)
+    like = post.likes.where(user_id:current_user.id)
+    like.first.destroy
     redirect_to posts_path(anchor: "post-#{post.id}")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :posts, shallow: true do
-    resources :likes, only: :create
+    resources :likes, only: %i[create destroy]#, only: :create,
     resources :comments, except: %i[index show] do
       resources :replies, except: %i[index new show]
     end
