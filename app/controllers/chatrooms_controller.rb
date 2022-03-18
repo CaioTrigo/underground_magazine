17a25ec489889c3975e67f25ebe909619777cb1d
Gabriel Schiavo <gabriel.penha@agrotools.com.br>
2022-03-18 17:16:31 -0300

    remove debug pry

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class ChatroomsController < ApplicationController
   def index
     @chatrooms = Chatroom.all
@@ -15,7 +14,6 @@ class ChatroomsController < ApplicationController
     @chatroom.user_id = current_user.id
     @chatroom.first_user_id = current_user.id
     @chatroom.second_user_id = recipient.id
-    binding.pry
     if @chatroom.save
       redirect_to chatroom_path(@chatroom.id)
     end
